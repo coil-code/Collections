37dd7a51bfb4d10cfa716e5fb0850d5b84751837
coil-code <coil.code@gmail.com>
2020-09-30 10:30:47 +0300

    Improve DefaultUserParameterStorage

--- Common/IOS/Source/UserParameter/DefaultUserParameterStorage.swift
+++ Common/IOS/Source/UserParameter/DefaultUserParameterStorage.swift
@@ -12,17 +12,11 @@ class DefaultUserParameterStorage: UserParameterStorage {
 
   static func makeStandard() -> DefaultUserParameterStorage { DefaultUserParameterStorage(underlying: UserDefaults.standard) }
 
-  func string(forKey key: String) -> String? {
-    guard let object = underlying.object(forKey: key) else {
-      return nil
-    }
-    return (object as! String)
-  }
-
-  func integer(forKey key: String) -> Int? { numberObject(forKey: key)?.intValue }
-  func single(forKey key: String) -> Float? { numberObject(forKey: key)?.floatValue }
-  func double(forKey key: String) -> Double? { numberObject(forKey: key)?.doubleValue }
-  func boolean(forKey key: String) -> Bool? { numberObject(forKey: key)?.boolValue }
+  func string(forKey key: String) -> String? { object(forKey: key) }
+  func integer(forKey key: String) -> Int? { (object(forKey: key) as NSNumber?)?.intValue }
+  func single(forKey key: String) -> Float? { (object(forKey: key) as NSNumber?)?.floatValue }
+  func double(forKey key: String) -> Double? { (object(forKey: key) as NSNumber?)?.doubleValue }
+  func boolean(forKey key: String) -> Bool? { (object(forKey: key) as NSNumber?)?.boolValue }
 
   func set(_ value: String, forKey key: String) { underlying.set(value, forKey: key) }
   func set(_ value: Int, forKey key: String) { underlying.set(NSNumber(value: value), forKey: key) }
@@ -30,11 +24,11 @@ class DefaultUserParameterStorage: UserParameterStorage {
   func set(_ value: Double, forKey key: String) { underlying.set(NSNumber(value: value), forKey: key) }
   func set(_ value: Bool, forKey key: String) { underlying.set(NSNumber(value: value), forKey: key) }
 
-  private func numberObject(forKey key: String) -> NSNumber? {
+  private func object<T>(forKey key: String) -> T? {
     guard let object = underlying.object(forKey: key) else {
       return nil
     }
-    return (object as! NSNumber)
+    return (object as! T)
   }
 
 }
